813a851e2cd6b2659f984ed454bc841772495246
Benny <bennyshijianhui@hotmail.com>
2018-08-21 01:13:48 +1000

    add feature - retrieve info

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from mongoengine import *
 from flask import *
 import course_db
+import re
+import json
 
 app = Flask(__name__)
 
@@ -20,10 +22,22 @@ def delete_course_db():
 @app.route('/courses/<code>', methods=['GET'])
 def get_course_info(code):
     connect(host='mongodb://benny:comp9900@ds125912.mlab.com:25912/comp9900')
+    raw_query = request.query_string.decode()
+    query = re.split('\+', raw_query)
+
     for course in course_db.Course.objects:
         if course.code == code:
-            return course.to_json(), 200
-    return code + " not found", 404
+            if len(raw_query) == 0:
+                result = json.loads(course.to_json())
+            else:
+                result = {}
+                info = json.loads(course.to_json())
+                for key in query:
+                    if key not in course:
+                        return "Wrong query.", 404
+                    result[key] = info[key]
+            return jsonify(result), 200
+    return code + " not found.", 404
 
 
 if __name__ == '__main__':
