18513f9698896d071f72dbf070f7925b3a1aa0fc
barbarianJ <kenosis2327@yahoo.com>
2018-08-24 13:05:11 +1000

    merged jin & benny, tested

--- bot.py
+++ bot.py
@@ -1,12 +1,20 @@
 from nlp.rasa import RasaNLP
+from db.retrieve_info import get_course_info
 
 
+q = 'Who is the pre_requisite of COMP9417?'
 
+
+#  train the nlp model
 nlp = RasaNLP('./rasa-config.json', './rasa-data.json', './rasa-model')
-
 nlp.train()
 
 
-print(nlp.find_reply('Who is the tutor of COMP9417?'))
-res = nlp.parse('Who is the LiC of COMP9417?')
-print(res)
+# parse query
+res = nlp.find_reply(q)
+need_reply, [deter, table, [keyword], att] = res
+
+
+# get the answer
+answer = get_course_info(table, (keyword).upper(), att)
+
