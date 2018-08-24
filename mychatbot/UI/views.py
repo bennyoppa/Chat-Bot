7a08793f1aea97507992157ca7814ba376fd7fb7
ChrisYANG <tmac10261111@gmail.com>
2018-08-24 14:43:09 +1000

    COMMIT

--- mychatbot/UI/views.py
+++ mychatbot/UI/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render,HttpResponse
+
+
+import sys
+sys.path.append('../../../')
+
+from nlp.rasa import RasaNLP
+from db.retrieve_info import get_course_info
 import json
 
 # Create your views here.
@@ -9,9 +16,19 @@ def homepage(request):
 
 def Chatbot_sub(req):
      result='Sup!!!'
+
+     nlp = RasaNLP('./rasa-config.json', './rasa-data.json', './rasa-model')
+     nlp.train()
+
      if req.method == 'POST':
           print(req.body)
           data = json.loads(req.body)
           print(data)
-          return HttpResponse(json.dumps(result), content_type='application/json')
+          res = nlp.find_reply(data['content'])
+          need_reply, [deter, table, [keyword], att] = res
+          answer = get_course_info(table, (keyword).upper(), att)
+          reply = ''
+          for q in answer:
+               reply += answer[q] + '\n'
+          return HttpResponse(json.dumps(reply), content_type='application/json')
      return render(req, 'error.html')
