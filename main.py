f1ce2923a14610d60b24e92dae76960decd40750
Benny <bennyshijianhui@hotmail.com>
2018-08-24 12:39:49 +1000

    split code into 2 files (main.py and retrieve_info.py)

--- main.py
+++ main.py
@@ -1,43 +1,5 @@
-from mongoengine import *
-import course_db
+from retrieve_info import *
 import re
-import json
-
-
-def create_course_db():
-    course_db.add_courses()
-    return "Course database created."
-
-
-def delete_course_db():
-    course_db.delete_all_courses()
-    return "Course database deleted."
-
-
-def get_course_info(table, keyword, query):
-    connect(host='mongodb://benny:comp9900@ds125912.mlab.com:25912/comp9900')
-    if table == 'course':
-        target = course_db.Course.objects
-    elif table == 'stream':
-        target = course_db.Course.objects
-    elif table == 'staff':
-        target = course_db.Course.objects
-    else:
-        return "Wrong table name."
-
-    for doc in target:
-        if doc._id == keyword:
-            if len(query) == 0:
-                result = json.loads(doc.to_json())
-            else:
-                result = {}
-                info = json.loads(doc.to_json())
-                for key in query:
-                    if key not in query:
-                        return "Wrong desired output."
-                    result[key] = info[key]
-            return result
-    return "Not found."
 
 
 if __name__ == '__main__':
