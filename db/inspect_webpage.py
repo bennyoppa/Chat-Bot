b129f56d787d21d8313fd527249fc1b267bf3902
Benny <bennyshijianhui@hotmail.com>
2018-08-27 17:23:13 +1000

    update

--- db/inspect_webpage.py
+++ db/inspect_webpage.py
@@ -1,7 +1,12 @@
 from bs4 import BeautifulSoup
 import requests
+from db.info_lists import info
 
-url = 'http://timetable.unsw.edu.au/2018/COMP9021.html'
+
+handbooks, timetables, course_codes, course_names, adk = info()
+
+
+url = 'http://www.handbook.unsw.edu.au/postgraduate/courses/2019/COMP9900'
 page = requests.get(url)
 soup = BeautifulSoup(page.content, "html.parser")
 print(soup)
