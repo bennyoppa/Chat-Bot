e3377a3ead5ee52e755b40f70aa5b16311ab82ad
Benny <bennyshijianhui@hotmail.com>
2018-08-28 12:59:26 +1000

    fix bugs for Lecturer in Charge

--- db/course_db.py
+++ db/course_db.py
@@ -10,16 +10,16 @@ class Course(Document):
     adk = BooleanField(required=False)
     pre_requisite = StringField(required=False)
     description = StringField(required=False)
-    # lic = StringField(required=False)
+    lic = StringField(required=False)
 
-    def __init__(self, _id, name, adk, pre_requisite, description, *args, **values):
+    def __init__(self, _id, name, adk, pre_requisite='N/A', description='N/A', lic='N/A', *args, **values):
         super().__init__(*args, **values)
         self._id = _id
         self.name = name
         self.adk = adk
         self.pre_requisite = pre_requisite
         self.description = description
-        # self.lic = lic
+        self.lic = lic
 
 
 def add_courses():
@@ -28,8 +28,9 @@ def add_courses():
 
     for i in range(number_courses):
         print(course_codes[i])
-        pre_req = ''
-        description = ''
+        pre_req = 'N/A'
+        description = 'N/A'
+        lic = 'N/A'
 
         page = requests.get('http://www.handbook.unsw.edu.au/postgraduate/courses/2019/' + course_codes[i])
         soup = BeautifulSoup(page.content, "html.parser")
@@ -41,19 +42,15 @@ def add_courses():
             description = soup.find('div', class_="a-card-text m-toggle-text has-focus").get_text()
             description = description.replace('\n', '')
 
-        # url = timetables[i]
-        # if url:
-        #     page = requests.get(url)
-        #     soup = BeautifulSoup(page.content, "html.parser")
-        #     if soup.find(text='T1'):
-        #         lic = soup.find(text='T1').find_next().get_text()
-        #     elif soup.find(text='T2'):
-        #         lic = soup.find(text='T2').find_next().get_text()
-        #     else:
-        #         lic = None
-        # else:
-        #     lic = None
+        url = timetables[i]
+        if url:
+            page = requests.get(url)
+            soup = BeautifulSoup(page.content, "html.parser")
+            if soup.find(text='T1'):
+                lic = soup.find(text='T1').find_next().get_text()
+            elif soup.find(text='T2'):
+                lic = soup.find(text='T2').find_next().get_text()
 
         connect(host='mongodb://benny:comp9900@ds125912.mlab.com:25912/comp9900')
-        course = Course(course_codes[i], course_names[i], adk[i], pre_req, description)
+        course = Course(course_codes[i], course_names[i], adk[i], pre_req, description, lic)
         course.save()
