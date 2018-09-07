6a98b2683be1e5192679434746074f74602df7d6
Benny <bennyshijianhui@hotmail.com>
2018-09-07 13:35:11 +1000

    keywords accepts a list

--- db/retrieve_info.py
+++ db/retrieve_info.py
@@ -14,56 +14,61 @@ def create_stream_db():
     return "Stream database created."
 
 
-def get_info(table, keyword, query):
+def get_info(table, keywords, query):
     if table == 'course':
-        return get_course_info(keyword, query)
+        return get_course_info(keywords, query)
     elif table == 'stream':
-        return get_stream_info(keyword, query)
+        return get_stream_info(keywords, query)
     elif table == 'staff':
         pass
     return None
 
 
-def get_course_info(keyword, query):
+def get_course_info(keywords, query):
     connect(host='mongodb://benny:comp9900@ds125912.mlab.com:25912/comp9900')
     table = course_db.Course.objects
+    final_result = []
 
-    for doc in table:
-        if doc._id == keyword:
-            if len(query) == 0:
-                result = json.loads(doc.to_json())
-            else:
-                result = {}
-                info = json.loads(doc.to_json())
-                for key in query:
-                    if key not in query:
-                        return None
-                    result[key] = info[key]
-            return result
-    return None
+    for keyword in keywords:
+        result = {}
+        for doc in table:
+            if doc._id == keyword:
+                if len(query) == 0:
+                    result = json.loads(doc.to_json())
+                else:
+                    info = json.loads(doc.to_json())
+                    for key in query:
+                        result[key] = info[key]
+        final_result.append(result)
+    return final_result
 
 
-def get_stream_info(keyword, query):
+def get_stream_info(keywords, original_query):
     connect(host='mongodb://benny:comp9900@ds125912.mlab.com:25912/comp9900')
     table = stream_db.Stream.objects
+    final_result = []
 
-    for doc in table:
-        if doc._id == keyword:
-            if len(query) == 0:
-                return json.loads(doc.to_json())['areas']
-            else:
-                areas = json.loads(doc.to_json())['areas']
-                while query:
-                    course = query.pop(0)
-                    for i in range(len(areas)):
-                        area = areas[i]['electives']
-                        for j in range(len(area)):
-                            if course == area[j]:
-                                areas[i]['electives'].pop(j)
-                                areas[i]['number'] = areas[i]['number'] - 1
-                                break
-                result = []
-                for k in range(len(areas)):
-                    if areas[k]['number'] != 0:
-                        result.append(areas[k])
-                return result
+    for keyword in keywords:
+        result = []
+        query = [x for x in original_query]
+        for doc in table:
+            if doc._id == keyword:
+                if len(query) == 0:
+                    result = json.loads(doc.to_json())['areas']
+                else:
+                    areas = json.loads(doc.to_json())['areas']
+                    while query:
+                        course = query.pop(0)
+                        for i in range(len(areas)):
+                            area = areas[i]['electives']
+                            for j in range(len(area)):
+                                if course == area[j]:
+                                    areas[i]['electives'].pop(j)
+                                    areas[i]['number'] = areas[i]['number'] - 1
+                                    break
+
+                    for k in range(len(areas)):
+                        if areas[k]['number'] != 0:
+                            result.append(areas[k])
+        final_result.append(result)
+    return final_result
